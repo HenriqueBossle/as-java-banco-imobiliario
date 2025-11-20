22e3e6b766a5817c4b8c2ac502ff20439a190134
Henrique Bossle <henriquegoncalvesbossle@gmail.com>
2025-11-20 15:40:24 -0300

    imposto add

---
+++ src/Imposto.java
@@ -0,0 +1,14 @@
+public class Imposto extends Casa{
+    private double taxa;
+
+    public Imposto(String nome, int posicao, double taxa) {
+        super(nome, posicao);
+        this.taxa = taxa;
+    }
+
+    @Override
+    public void acao(Jogador j){
+        System.out.println(j.getNome() + "deve pagar imposto de R$ ");
+        j.pagar(taxa);
+    }
+}
