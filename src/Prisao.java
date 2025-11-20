b53e0aa5ada3f0118bd1b82c607ab19a138bf48d
Henrique Bossle <henriquegoncalvesbossle@gmail.com>
2025-11-20 19:33:35 -0300

    prisao

--- src/Prisao.java
+++ src/Prisao.java
@@ -1,7 +1,15 @@
-public class Prisao {
-    Jogador jogador;
+public class Prisao extends Casa{
+    private int rodadasPreso;
 
-    public Prisao(Jogador j) {
-        
+    public Prisao(String nome, int posicao, int rodadasPreso)
+    {
+        super(nome, posicao);
+        this.rodadasPreso = rodadasPreso;
+    }
+
+    @Override
+    public void acao(Jogador j){
+        System.out.println(j.getNome() + "foi preso, ficara 3 rodadas sem jogar");
+
     }
 }
